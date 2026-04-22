550b2a27a5782d037e1d4e927593917729526c7c
fietsvoeter <olavvdwal@gmail.com>
2026-04-22 09:40:00 +0200

    Add src/app/robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,5 @@
+import { MetadataRoute } from 'next'
+import { SITE_URL } from '@/lib/mdx'
+export default function robots(): MetadataRoute.Robots {
+  return { rules: [{ userAgent: '*', allow: '/', disallow: ['/api/'] }], sitemap: `${SITE_URL}/sitemap.xml` }
+}
