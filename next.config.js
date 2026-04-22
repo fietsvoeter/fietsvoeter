beb81cc56b3daba864a1d6d41c18881196f63011
fietsvoeter <olavvdwal@gmail.com>
2026-04-22 13:52:52 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   images: {
     formats: ['image/webp'],
