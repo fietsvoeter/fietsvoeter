7bf901bcb3b43661ab433373cf957d785d5e5264
fietsvoeter <olavvdwal@gmail.com>
2026-04-23 13:15:04 +0200

    SVG fix: next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  images: { formats: ['image/webp'] },
+  images: {
+    formats: ['image/webp'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 }
 module.exports = nextConfig
